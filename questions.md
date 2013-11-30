73379fc58f2840c0e3469bb07571c614557dd1b6
Bernie Chiu <bernie_chiu@hotmail.com>
2013-11-30 21:29:05 +0800

    Finish Array test

--- questions.md
+++ questions.md
@@ -1,21 +1,36 @@
 ## Array Docs
 
-0) Link to the Array documentation:
+0) Link to the Array documentation: 
+
+[Array](http://www.ruby-doc.org/core-1.9.3/Array.html)
 
 1) Direct Link to the `Array#delete_if` method:
 
+[Array#delete_if](http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-delete_if)
+
 2) Link to `Fixnum` docs:
+
+[Fixnum](http://www.ruby-doc.org/core-1.9.3/Fixnum.html)
 
 3) Link to `Fixnum#even?` docs:
 
+[Fixnum#even?](http://www.ruby-doc.org/core-1.9.3/Fixnum.html#method-i-even-3F)
+
 4) Link to `Hash` docs:
+
+[Hash](http://www.ruby-doc.org/core-1.9.3/Hash.html)
 
 5) Link to `Hash.keys` docs:
 
+[Hash.keys](http://www.ruby-doc.org/core-1.9.3/Hash.html#method-i-keys)
+
 6) Link to `String` docs:
+
+[String](http://ruby-doc.org/core-2.0.0/String.html)
 
 7) Link to `String#upcase docs:
 
+[String#upcase](http://ruby-doc.org/core-2.0.0/String.html#method-i-upcase)
 
 ## Active Record Docs
 
--- tests/array_test.rb
+++ tests/array_test.rb
@@ -3,20 +3,21 @@ require "test/unit"
 class ArrayTest < Test::Unit::TestCase
 
   def test_true
-    assert false
+    assert true
   end
 
   def test_reject
     array = [1,2,3,4,5,6,7,8]
     # put your code here
+    array = array.reject {|x| x.even?}
     assert_equal [1,3,5,7], array
   end
-
 
   # write code that returns the first three elements of the array
   def test_first_three
     array = [1,2,3,4,5]
     # put your code here
+    array = array.first(3)
     assert_equal [1,2,3], array
   end
 
@@ -24,6 +25,7 @@ class ArrayTest < Test::Unit::TestCase
   def test_last_three
     array = [1,2,3,4,5]
     # put your code here
+    array = array.last(3)
     assert_equal [3,4,5], array
   end
 
@@ -31,7 +33,8 @@ class ArrayTest < Test::Unit::TestCase
   def test_map
     array = [1,2,3,4,5,6,7,8]
     # put your code here
-    assert_equal [2, 4, 6, 8, 10, 12, 14, 16] , array
+    array = array.map {|num| num * 2}
+    assert_equal [2, 4, 6, 8, 10, 12, 14, 16], array
   end
 
   # Fix this test using only Array#map!
@@ -42,6 +45,7 @@ class ArrayTest < Test::Unit::TestCase
   def test_map_bang
     array = [1,2,3,4,5,6,7,8]
     # put your code here
+    array.map!{|num| (num * 2) - 1}
     assert_equal [1, 3, 5, 7, 9, 11, 13, 15] , array
   end
 
@@ -55,6 +59,7 @@ class ArrayTest < Test::Unit::TestCase
   def test_oposite_of_reject
     array = ["hello", 2, 3, "there", "how", 9, "are you"]
     # put your code here
+    array = array.reject{|x| !x.is_a?(String)}
     assert_equal ["hello", "there", "how", "are you"], array
   end
 
@@ -69,6 +74,7 @@ class ArrayTest < Test::Unit::TestCase
   def  test_array_append
     array = [1,2,3,4,5]
     # put your code here
+    array << 99
     assert_equal [1,2,3,4,5,99], array
   end
 
@@ -79,6 +85,7 @@ class ArrayTest < Test::Unit::TestCase
     array_one = [1,2,3,4,5]
     array_two = [6,7,8,9,10]
     # put your code here
+    array.concat(array_one).concat(array_two)
     assert_equal [1,2,3,4,5,6,7,8,9,10], array
   end
 
@@ -91,6 +98,7 @@ class ArrayTest < Test::Unit::TestCase
     array_one = [1,2,3,4,5]
     array_two = [2,5,9]
     # put your code here
+    array = array_one & array_two
     assert_equal [2,5], array
   end
 
@@ -102,6 +110,7 @@ class ArrayTest < Test::Unit::TestCase
     array_one = [1,2,3,4,5]
     array_two = [2,5,9]
     # put your code here
+    array = array_one - array_two
     assert_equal [1,3,4], array
   end
 
@@ -113,6 +122,7 @@ class ArrayTest < Test::Unit::TestCase
     array_one = [1,2,3,4,5]
     array_two = [2,5,9]
     # put your code here
+    array = array_two - array_one
     assert_equal [9], array
   end
 
@@ -121,6 +131,7 @@ class ArrayTest < Test::Unit::TestCase
   def test_fix_nested_array
     array = [1, [2], [3, [4,[5]]], 6]
     # put your code here
+    array.flatten!
     assert_equal [1,2,3,4,5,6], array
   end
 
@@ -129,6 +140,7 @@ class ArrayTest < Test::Unit::TestCase
     array = []
     array_one = [1, nil, 2, 3, 4, nil, 5, nil]
     # put your code here
+    array = array_one.compact
     assert_equal [1,2,3,4,5], array
   end
 
@@ -139,6 +151,7 @@ class ArrayTest < Test::Unit::TestCase
     num_of_elem = nil
     array = [1,2,3,4]
     # put your code here
+    num_of_elem = array.count
     assert_equal 4, num_of_elem
   end
 
